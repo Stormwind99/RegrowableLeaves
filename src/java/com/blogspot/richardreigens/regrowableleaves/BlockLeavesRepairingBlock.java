5b9fde4c92f60c069e71e48b57709b7a16e5ce06
Jason Spangler <jason.spangler@descendentstudios.com>
2018-06-03 22:09:13 -0500

    Repairing blocks invisible unless debug mode on, markDirty calls, general cleanup

--- src/java/com/blogspot/richardreigens/regrowableleaves/BlockLeavesRepairingBlock.java
+++ src/java/com/blogspot/richardreigens/regrowableleaves/BlockLeavesRepairingBlock.java
@@ -2,7 +2,9 @@ package com.blogspot.richardreigens.regrowableleaves;
 
 import com.wumple.blockrepair.BlockRepairingBlock;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumBlockRenderType;
 import net.minecraft.world.World;
 
 public class BlockLeavesRepairingBlock extends BlockRepairingBlock {
@@ -10,5 +12,17 @@ public class BlockLeavesRepairingBlock extends BlockRepairingBlock {
     public TileEntity createNewTileEntity(World var1, int meta) 
     {
     	return new TileEntityLeavesRepairingBlock();
+    }
+    
+    @Override
+    public EnumBlockRenderType getRenderType(IBlockState state)
+    {
+    	// show debug model if debugging, invisible if not debugging
+    	if (ConfigurationHandler.generalSettings.debugMode == true) {
+    		return EnumBlockRenderType.MODEL;
+    	} 
+    	else {
+    		return EnumBlockRenderType.INVISIBLE;
+    	}
     }
 }
